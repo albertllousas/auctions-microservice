2bbcf94b251a2ef1a011d8a4c456d259cf7f405d
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2023-03-31 16:52:10 +0200

    Fix tests

--- src/main/kotlin/auction/infrastructure/out/http/UsersHttpClient.kt
+++ src/main/kotlin/auction/infrastructure/out/http/UsersHttpClient.kt
@@ -7,9 +7,7 @@ import auction.domain.model.FindUser
 import auction.domain.model.User
 import auction.domain.model.UserId
 import auction.domain.model.UserNotFound
-import com.fasterxml.jackson.databind.DeserializationFeature.*
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import okhttp3.Call
 import okhttp3.OkHttpClient
@@ -25,7 +23,7 @@ data class UsersHttpClient(
     private val okHttpClient: OkHttpClient,
     private val baseUrl: String,
     private val mapper: ObjectMapper
-    ) {
+) {
 
     private val getUsersUrl = "$baseUrl/users"
 
